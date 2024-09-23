33434d5d94911e660ac84a639e9760fd3f5056d0
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-23 21:17:28 +0430

    Flight Management System in Laravel React API

---
+++ database/migrations/2024_02_03_065629_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('course');
+            $table->string('email');
+            $table->Integer('fees');
+            $table->Integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
